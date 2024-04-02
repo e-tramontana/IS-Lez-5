7bb71fb492bd1e3e0c14f770574ad7930f3f0f3a
E. Tramontana <73878438+e-tramontana@users.noreply.github.com>
2024-04-02 16:25:56 +0200

    MainEsami usa istanza di StCreator

--- MainEsami.java
+++ MainEsami.java
@@ -3,12 +3,14 @@
  */
 public class MainEsami {
     public static void main(String[] args) {
-        Client c = new Client();
+        Creator crea = new StCreator();
+        Client c = new Client(crea);
         c.registraUnEsame();
         c.sequenzaOperazioni();
 
         IStudente s1 = c.getStudente();
         s1.nuovoEsame("Geo", 9);
         s1.getMedia();
+        System.out.println("num istanze " + crea.getNum());
     }
 }
