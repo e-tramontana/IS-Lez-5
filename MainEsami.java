d5b3147fe064655454d5aab2f9bc67b4d4e78451
E. Tramontana <73878438+e-tramontana@users.noreply.github.com>
2024-04-02 16:52:13 +0200

    Update MainEsami.java

--- MainEsami.java
+++ MainEsami.java
@@ -8,9 +8,14 @@ public class MainEsami {
         c.registraUnEsame();
         c.sequenzaOperazioni();
 
-        IStudente s1 = c.getStudente();
+        IStudente s1 = c.get();
         s1.nuovoEsame("Geo", 9);
         s1.getMedia();
+
+        IStudente s2 = crea.creaStudente();
+        s2.nuovoEsame("Italian", 10);
+        s2.getMedia();
+        
         System.out.println("num istanze " + crea.getNum());
     }
 }
